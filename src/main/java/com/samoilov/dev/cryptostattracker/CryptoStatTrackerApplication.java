45e088477e8439a18615e71e05e3371c6d6bc29a
vladimir.samoilov <vladimir.samoilov@ecodef.cz>
2024-10-12 10:52:52 +0200

    - init commit

---
+++ src/main/java/com/samoilov/dev/cryptostattracker/CryptoStatTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.samoilov.dev.cryptostattracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoStatTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoStatTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samoilov/dev/cryptostattracker/CryptoStatTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samoilov.dev.cryptostattracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoStatTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
